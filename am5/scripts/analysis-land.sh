5c57faf042b35cd09465e85ac02da32bfe7c097d
Chris Blanton <Chris.Blanton@an106.princeton.rdhpcs.noaa.gov>
2024-10-23 10:43:14 -0400

    updated analysis-land driver script for discussion

--- am5/scripts/analysis-land.sh
+++ am5/scripts/analysis-land.sh
@@ -3,6 +3,7 @@
 # set path to catalog and output directory
 catalog=
 out_dir=
+config_yaml=
 
 # Load modules.
 source /usr/local/Modules/5.1.1/init/bash
@@ -10,4 +11,5 @@ module load python/3.9
 
 # Run the analysis script.
 source $CYLC_WORKFLOW_SHARE_DIR/analysis-envs/land/bin/activate
-python3 -c "from freanalysis_land import LandAnalysisScript; LandAnalysisScript().run_analysis('$catalog', '$out_dir')"
+python3 -c "from analysis_scripts import AnalysisScript; run_analysis(
+    'freanalysis_clouds', '$catalog', '$out_dir', $config_yaml)"
